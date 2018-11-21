c63e0fad7d230b547e7af7e30be10d7e1db6dac5
m-aditya <adityamenon1996@gmail.com>
2018-11-21 10:13:29 +0530

    Added Splash Screen

--- dashboard/main.js
+++ dashboard/main.js
@@ -21,13 +21,16 @@ app.on ('ready', function () {
     width: 1920,
     height: 1080,
     show: false // don't show the main window
+    
 });
+mainWindow.setBackgroundColor("#1a2035");
 // create a new `splash`-Window 
-splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true});
+splash = new BrowserWindow({width: 800, height: 600, transparent: true, frame: false, alwaysOnTop: true});
 splash.loadURL(`file://${__dirname}/splash.html`);
+
 mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-var delayInMilliseconds = 5000; //1 second
+var delayInMilliseconds = 5000; 
 
 // if main window is ready to show, then destroy the splash window and show up the main window
 mainWindow.once('ready-to-show', () => {
